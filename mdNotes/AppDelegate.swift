1f9a7d0cf9cd2d06a1b4c3f1906400db33a12ce6
Austin Chiatto <chiattoaustin@gmail.com>
2024-05-31 14:42:16 -0700

    Improved flexibility

--- mdNotes/AppDelegate.swift
+++ mdNotes/AppDelegate.swift
@@ -9,85 +9,125 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    var statusItem: NSStatusItem?
     var recentCharacters = [String]()
-
+    
+    let mdCommands: [String: [CGKeyCode]] = [
+        "# ": [CGKeyCode(56), CGKeyCode(55), CGKeyCode(17)],                // ⇧ ⌘ T
+        "## ": [CGKeyCode(56), CGKeyCode(55), CGKeyCode(4)],                // ⇧ ⌘ H
+        "### ": [CGKeyCode(56), CGKeyCode(55), CGKeyCode(38)],              // ⇧ ⌘ J
+        "#### ": [CGKeyCode(56), CGKeyCode(55), CGKeyCode(11)],             // ⇧ ⌘ B
+        "``` ": [CGKeyCode(56), CGKeyCode(55), CGKeyCode(46)],              // ⇧ ⌘ M
+        "> ": [CGKeyCode(55), CGKeyCode(39)],                               // ⌘'
+        "[] ": [CGKeyCode(56), CGKeyCode(55), CGKeyCode(37)],               // ⇧ ⌘ L
+    ]
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-        startMonitoringKeystrokes()
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
+            setupStatusItem()
+            checkAccessibilityPermissions()
+            startGlobalMonitoringKeystrokes()
+        }
     
-    // Start monitoring keystrokes
-    func startMonitoringKeystrokes() {
+    func setupStatusItem() {
+            statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+            statusItem?.button?.title = "mdNotes"
+            let menu = NSMenu()
+            menu.addItem(NSMenuItem(title: "Quit", action: #selector(quit), keyEquivalent: "q"))
+            statusItem?.menu = menu
+        }
+
+        @objc func quit() {
+            NSApplication.shared.terminate(self)
+        }
+    
+    func checkAccessibilityPermissions() {
+            DispatchQueue.main.async {
+                let options: NSDictionary = [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as NSString: true]
+                let accessEnabled = AXIsProcessTrustedWithOptions(options)
+
+                if !accessEnabled {
+                    print("Accessibility permissions are not enabled. Prompting user for permissions.")
+                } else {
+                    print("Accessibility permissions are enabled.")
+                }
+            }
+        }
+    
+    func startGlobalMonitoringKeystrokes() {
         NSEvent.addGlobalMonitorForEvents(matching: .keyUp) { [weak self] event in
             self?.handleKeyUpEvent(event)
         }
-        
-        NSEvent.addLocalMonitorForEvents(matching: .keyUp) { [weak self] event in
-            self?.handleKeyUpEvent(event)
-            return event
-        }
     }
+    
     
     func handleKeyUpEvent(_ event: NSEvent) {
-        guard let characters = event.characters else { return }
+        guard let characters = event.characters, let notesApp = NSWorkspace.shared.frontmostApplication, notesApp.bundleIdentifier == "com.apple.Notes" else {
+            return
+        }
         
-        if let frontApp = NSWorkspace.shared.frontmostApplication,
-           frontApp.bundleIdentifier == "com.apple.Notes" {
-            recentCharacters.append(contentsOf: characters.map { String($0) })
-            if recentCharacters.count > 3 {
-                recentCharacters.removeFirst(recentCharacters.count - 3)
-            }
-            
-            if recentCharacters.joined() == "## " {
-                simulateBackspace(count: 3)
-                triggerKeyboardShortcut()
+        recentCharacters.append(contentsOf: characters.map { String($0) })
+        
+        let maxRecentCharacters = 5
+        if recentCharacters.count > maxRecentCharacters {
+            recentCharacters.removeFirst(recentCharacters.count - maxRecentCharacters)
+        }
+
+        let recentString = recentCharacters.joined()
+        for (mdCommand, shortcutKeys) in mdCommands {
+            if recentString.hasSuffix(mdCommand) {
+                triggerFormatting(keys: shortcutKeys)
                 recentCharacters.removeAll()
+                break
             }
         }
     }
     
-    func simulateBackspace(count: Int) {
-        let backspace = CGEvent(keyboardEventSource: nil, virtualKey: CGKeyCode(51), keyDown: true)
-        let backspaceUp = CGEvent(keyboardEventSource: nil, virtualKey: CGKeyCode(51), keyDown: false)
-        
-        for _ in 0..<count {
-            backspace?.post(tap: .cghidEventTap)
-            backspaceUp?.post(tap: .cghidEventTap)
+    // Trigger specified Apple Notes formatting shortcut
+    func triggerFormatting(keys: [CGKeyCode]) {
+        print("Triggering formatting for keys: \(keys)")
+
+        let source = CGEventSource(stateID: .hidSystemState)
+        let keyFlags: CGEventFlags = [.maskShift, .maskCommand]
+
+        func createKeyEvent(key: CGKeyCode, keyDown: Bool) -> CGEvent? {
+            return CGEvent(keyboardEventSource: source, virtualKey: key, keyDown: keyDown)
+        }
+
+        // Press down all modifier keys
+        keys.forEach { key in
+            if let event = createKeyEvent(key: key, keyDown: true) {
+                event.flags = keyFlags
+                event.post(tap: CGEventTapLocation.cghidEventTap)
+            }
+        }
+
+        // Release all modifier keys
+        keys.reversed().forEach { key in
+            if let event = createKeyEvent(key: key, keyDown: false) {
+                event.flags = keyFlags
+                event.post(tap: CGEventTapLocation.cghidEventTap)
+            }
         }
     }
-    
-    // Function to trigger the "Shift + Command + J" keyboard shortcut
-        func triggerKeyboardShortcut() {
-            let shiftDown = CGEvent(keyboardEventSource: nil, virtualKey: CGKeyCode(56), keyDown: true)
-            let cmdDown = CGEvent(keyboardEventSource: nil, virtualKey: CGKeyCode(55), keyDown: true)
-            let jKeyDown = CGEvent(keyboardEventSource: nil, virtualKey: CGKeyCode(38), keyDown: true)
-            let jKeyUp = CGEvent(keyboardEventSource: nil, virtualKey: CGKeyCode(38), keyDown: false)
-            let cmdUp = CGEvent(keyboardEventSource: nil, virtualKey: CGKeyCode(55), keyDown: false)
-            let shiftUp = CGEvent(keyboardEventSource: nil, virtualKey: CGKeyCode(56), keyDown: false)
 
-            shiftDown?.flags = [.maskShift]
-            cmdDown?.flags = [.maskCommand]
-            jKeyDown?.flags = [.maskCommand, .maskShift]
-            jKeyUp?.flags = [.maskCommand, .maskShift]
-            cmdUp?.flags = [.maskCommand]
-            shiftUp?.flags = [.maskShift]
-
-            shiftDown?.post(tap: .cghidEventTap)
-            cmdDown?.post(tap: .cghidEventTap)
-            jKeyDown?.post(tap: .cghidEventTap)
-            jKeyUp?.post(tap: .cghidEventTap)
-            cmdUp?.post(tap: .cghidEventTap)
-            shiftUp?.post(tap: .cghidEventTap)
-        }
 
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
+           return true
+       }
+
+       func applicationShouldRestoreApplicationState(_ app: NSApplication) -> Bool {
+           return false
+       }
+
+       func applicationShouldSaveApplicationState(_ app: NSApplication) -> Bool {
+           return false
+       }
+}
+
+extension Optional {
+    func apply(_ transform: (Wrapped) throws -> Void) rethrows -> Wrapped? {
+        if let value = self { try transform(value) }
+        return self
     }
 }
 
